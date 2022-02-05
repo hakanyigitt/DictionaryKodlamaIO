09f4c9e89512d894ad81abbf2d04d7b489706593
Hakan YİĞİT <hakanyigit028@hotmail.com>
2022-02-06 00:47:44 +0300

    Add project files.

---
+++ DictionaryKodlamaIO/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryKodlamaIO
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        TKey[] tempKeys;
+        TValue[] tempValues;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+        public void Show()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key : " + keys[i] + " Value : " + values[i]);
+            }
+        }
+    }
+}
---
+++ DictionaryKodlamaIO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryKodlamaIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenci = new MyDictionary<int, string>();
+            Console.WriteLine(ogrenci.Count);
+            ogrenci.Add(28, "Hakan");
+            ogrenci.Add(53, "Ahmet");
+            Console.WriteLine(ogrenci.Count);
+            ogrenci.Add(37, "Büşra");
+            ogrenci.Show();
+        }
+    }
+}
